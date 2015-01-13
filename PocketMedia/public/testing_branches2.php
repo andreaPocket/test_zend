e06442a848d554842a8c4284a6e1166179d2ecbd
andreaPocket <andrea@pocketmedia.mobi>
2015-01-13 10:46:57 -0500

    modifying the file testing_branches2

--- PocketMedia/public/testing_branches2.php
+++ PocketMedia/public/testing_branches2.php
@@ -1,3 +1,5 @@
 ,?php
 echo 'testing branches 2';
 //this file is to test the behaviour of branches, it will be created, added and commited from one branch and pushed from other
+//cambio dentro de feature1
+echo 'changes';
